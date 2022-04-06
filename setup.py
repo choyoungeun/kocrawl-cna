2556df48c645116bf7ef8971fad1308881e59ba5
zerosilver <youngcool0.0@gmail.com>
2022-04-06 13:20:01 +0900

    CNA project crawler commit. original_ver:kocrawl

---
+++ setup.py
@@ -0,0 +1,40 @@
+import codecs
+
+from setuptools import setup, find_packages
+
+
+def read_file(filename, cb):
+    with codecs.open(filename, 'r', 'utf8') as f:
+        return cb(f)
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kocrawl',
+    version='1.0.9',
+    description='Korean web crawler collections',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='(original : Hyunwoong Ko), Saebom Lee, Jeongyun Seo, Youngeun Cho',
+    author_email='gusdnd852@naver.com, 00springbom00@gmail.com, yooniverse@knu.ac.kr, youngcool0.0@gmail.com',
+    url='https://github.com/Kochat-framework/kocrawl',
+    packages=find_packages(exclude=[]),
+    keywords=['crawler', 'korean crawler', 'kochat'],
+    install_requires=read_file('requirements.txt', lambda f: list(
+        filter(bool, map(str.strip, f)))),
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)

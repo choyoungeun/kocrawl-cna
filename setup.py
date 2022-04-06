90bccf8a953abd36c089e68a654941dd85ff7025
zerosilver <youngcool0.0@gmail.com>
2022-04-06 23:54:54 +0900

    1.0 -> 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='kocrawl-cna',
-    version='1.0',
+    version='1.0.1',
     description='Korean web crawler collections',
     long_description=long_description,
     long_description_content_type='text/markdown',
